3e418f8b54bd0c877a6e84b850abd4ce953c90c8
NQ/IO <eshaan2031@icloud.com>
2026-01-13 09:49:14 -0600

    Update build.c

--- build/build.c
+++ build/build.c
@@ -1,72 +1,101 @@
-#include <assert.h>
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 
+int ipow (int begin, int power) {
+  int result = begin; // 10
+  for (int i = 1; i < power; ++i) {
+    result *= begin;
+  }
+}
+
 int stoi(char *str) {
   int result = 0;
   int i = 0;
   while (str[i] != '\0') {
-    result += (str[i] - '0') * pow(10, i);
+    result += (str[i] - '0') * ipow(10, i);
     i++;
   }
 
   return result;
 }
 
+void clean() {
+  remove("kernel.asm.cat");
+  remove("kernel.bin.cat");
+  remove("boot.bin");
+}
+
 int main(int argc, char **argv) {
   int filename_index = 0;
   int size_index = 0;
-  int seek = 0;
+  system("cat src/kernel.asm src/*[^kernel].asm > kernel.asm.cat");
 
   for (int i = 1; i < argc; ++i) {
     if (strcmp(argv[i], "-o") == 0) {
-      assert(argc > i);
+      if (argc > i){
+        cleanup();
+        return EXIT_FAILURE;
+      }
+      
       i++;
       filename_index = i;
       continue;
     }
 
     if (strcmp(argv[i], "-s") == 0) {
-      assert(argc > i);
+      if (argc > i) {
+        cleanup();
+        return EXIT_FAILURE;
+      }
+      
       i++;
       size_index = i;
-      assert(filename_index > 0);
+      if (filename_index > 0) {
+        cleanup();
+        return EXIT_FAILURE;
+      }
 
       char command[100];
       sprintf(command, "dd if=/dev/zero of=%s bs=512 count=%d",
               argv[filename_index], stoi(argv[size_index]));
 
       if (system(command) != 0) {
+        cleanup();
         return EXIT_FAILURE;
       }
 
       continue;
     }
-
-    assert(size_index > 0); // means the file has been made
-    char command[100];
-    sprintf(command, "nasm -f bin %s -o %s.bin", argv[i], argv[i]);
-
-    if (system(command) != 0) {
-      return EXIT_FAILURE;
-    }
-
-    struct stat st;
-    char bin_filename[strlen(argv[i]) + 4 + 1];
-    sprintf(bin_filename, "%s.bin", argv[i]);
-    stat(bin_filename, &st);
-    long long size = st.st_size;
-
-    sprintf(command, "dd if=%s.bin of=%s bs=512 seek=%d conv=notrunc", argv[i],
-            argv[filename_index], seek);
-    seek += size / 512;
-    if (size % 512 != 0) {
-      seek++;
-    }
-
-    remove(bin_filename);
   }
+
+  if (size_index > 0) {
+    cleanup();
+    return EXIT_FAILURE;
+  }
+  
+  if (system("nasm -f bin src/boot.asm -o boot.bin") != 0) {
+    cleanup();
+    return EXIT_FAILURE;
+  }
+  
+  if (system("nasm -f bin kernel.asm.cat -o kernel.bin.cat") != 0) {
+    cleanup();
+    return EXIT_FAILURE;
+  }
+  char command[200];
+  sprintf(command, "dd if=boot.bin of=%s bs=512 seek=0 conv=notrunc", arg[filename_index]);
+  if (system(command) != 0) {
+    cleanup();
+    return EXIT_FAILURE;
+  }
+  
+  sprintf(command, "dd if=kernel.bin.cat of=%s bs=512 seek=1 conv=notrunc", arg[filename_index]);
+  if (system(command) != 0) {
+    cleanup();
+    return EXIT_FAILURE;
+  }
+  
+  cleanup();
 }
