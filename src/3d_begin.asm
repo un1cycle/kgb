5a88d1ad30929aceb659c18fd7da341c6031616e
eshaan deshmukh <eshaan2031@icloud.com>
2026-01-21 16:58:55 +0000

    should have more accurate slope calculations now

--- src/3d_begin.asm
+++ src/3d_begin.asm
@@ -11,14 +11,12 @@ sub edx, ebx
 mov esi, ecx
 sub esi, eax
 
-push eax
-push edx
-mov eax, edi
-cdq
-idiv esi
-mov edi, eax
-pop edx
-pop eax
+; xmm0 = edi / esi
+; xmm0 used to be edi, so replace accordingly
+cvtsi2ss xmm0, edi
+cvtsi2ss xmm1, esi
+divss xmm0, xmm1
+; slope in xmm0
 
 mov esi, ebx
 push ecx
@@ -30,22 +28,23 @@ pop ecx
 ; rectangle should be from x1 -> x1 + 1
 ; y1 -> y1 + (y1 * slope + y-int)
 
-push eax ; 20 x1
-push ebx ; 16 y1
-push ecx ; 12 x2
-push edx ; 8 y2
-push edi ; 4 slope
+push eax ; 20->16 x1
+push ebx ; 16->12 y1
+push ecx ; 12->8 x2
+push edx ; 8->4 y2
 push esi ; 0 y-int
 
 mov esi, [lfb_addr]  
 mov edx, [lfb_pitch]  
 
 line_loop:
-mov eax, [esp + 16] ; y1
-mov ebx, [esp + 20] ; x1
-imul ebx, [esp + 4]
+mov eax, [esp + 12] ; y1
+mov ebx, [esp + 16] ; x1
+cvtsi2ss xmm1, ebx
+mulss xmm0, xmm1
+cvttss2si ebx, xmm0
 add ebx, [esp] ; now y2
-mov [esp + 16], ebx
+mov [esp + 12], ebx
 
 sub ebx, eax
 imul eax, edx
@@ -53,13 +52,13 @@ add esi, eax
 
 row_loop:
 mov edi, esi
-mov eax, [esp + 20]
-cmp eax, [esp + 12]
+mov eax, [esp + 16]
+cmp eax, [esp + 8]
 jge line_done
 
 mov ecx, eax
 inc ecx
-mov [esp + 20], ecx
+mov [esp + 16], ecx
 
 sub ecx, eax
 imul eax, 3
